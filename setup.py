59419b4c66f00eeea3e3cde76a1a5d0c8809eb79
Alex P <Opimenov@users.noreply.github.com>
2017-12-04 22:02:22 -0500

    and path

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     version = __version__,
     author = 'Michael Woods, mods Alex Pimenov',
     author_email = 'physicsmichael@gmail.com',
-    url = 'https://github.com/vgm64/gmplot',
+    url = 'https://github.com/Opimenov/gmplot',
     description = 'Provide a matplotlib like interface to plotting data with Google Maps',
     long_description=read('README.rst'),
     license='MIT',
